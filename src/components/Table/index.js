5693c05f52c25512a541f398997748b3dc6b171c
JGSims <josi3006@hotmail.com>
2020-03-28 02:44:48 -0400

    Added column headers as buttons with sort icons

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,10 +3,10 @@ import React from "react";
 function Table(props) {
     return <div className="container">
         <div className="row">
-            <div className="col-sm-2"><strong>Name</strong></div>
-            <div className="col-sm-3"><strong>Position</strong></div>
-            <div className="col-sm-2"><strong>Department</strong></div>
-            <div className="col-sm-2"><strong>Location</strong></div>
+            <div className="col-sm-2"><button className="sortBtn mr-2"><strong>Name</strong></button><i className="fas fa-sort-alpha-down"></i></div>
+            <div className="col-sm-3"><button className="sortBtn mr-2"><strong>Position</strong></button><i className="fas fa-sort-alpha-down"></i></div>
+            <div className="col-sm-2"><button className="sortBtn mr-2"><strong>Department</strong></button><i className="fas fa-sort-alpha-down"></i></div>
+            <div className="col-sm-2"><button className="sortBtn mr-2"><strong>Location</strong></button><i className="fas fa-sort-alpha-down"></i></div>
         </div>
         {props.children}
     </div>
